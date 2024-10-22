600c38c32eb2af7f93bcb16a36af7b627f847af1
nada-farid <nadafarid068@gmail.com>
2024-10-22 03:54:32 -0700

    save work

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -273,7 +273,7 @@
         slidesPerView: 4,
         spaceBetween: 30,
         speed: 1500,
-        loop: true,
+        loop: false,
 
         // Navigation arrows
 
@@ -361,35 +361,27 @@
 
 
 
- $(".cart-plus-minus").each(function () {
-    var $cartPlusMinus = $(this);
-    $cartPlusMinus.prepend('<div class="dec qtybutton">-</div>');
-    $cartPlusMinus.append('<div class="inc qtybutton">+</div>');
-
-    $cartPlusMinus.on("click", ".qtybutton", function () {
+    /*----------------------------
+        Cart Plus Minus Button
+    ------------------------------ */
+    var CartPlusMinus = $(".cart-plus-minus");
+    CartPlusMinus.prepend('<div class="dec qtybutton">-</div>');
+    CartPlusMinus.append('<div class="inc qtybutton">+</div>');
+    $(".qtybutton").on("click", function () {
         var $button = $(this);
-        var $input = $button.siblings(".cart-plus-minus-box");
-        var oldValue = parseFloat($input.val());
-        var productId = $input.data("product-id");
-
+        var oldValue = $button.parent().find("input").val();
         if ($button.text() === "+") {
-            var newVal = oldValue + 1;
+            var newVal = parseFloat(oldValue) + 1;
         } else {
             // Don't allow decrementing below zero
             if (oldValue > 1) {
-                var newVal = oldValue - 1;
+                var newVal = parseFloat(oldValue) - 1;
             } else {
                 newVal = 1;
             }
         }
-
-        $input.val(newVal);
-        updateQuantity(productId, newVal);
+        $button.parent().find("input").val(newVal);
     });
-});
-
-
-
 
 
     /*------------------------------
